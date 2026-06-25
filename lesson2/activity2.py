e0581aa203fd01a86de079117da268419a617990
soham <getsoham.j12@gmail.com>
2026-06-24 20:06:32 -0500

    lesson2

---
+++ lesson2/activity2.py
@@ -0,0 +1,13 @@
+# Creating different variables
+x = "Soham"
+y = 5
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
