c77fe75286dd6cae9aa2edec6a5224141f6565e2
soham <getsoham.j12@gmail.com>
2026-06-29 15:37:12 -0500

    Commit

---
+++ my_daily_routine_printer/routine.py
@@ -0,0 +1,15 @@
+print("My Daily Routine")
+# Step 1: Waking up
+print("I wake up at 6:30 AM.")
+
+# Step 2: Going to school
+print("I go to school at 7:45 AM.")
+
+# Step 3: After school activities
+print("I go to basketball practice after school.")
+
+# Step 4: Studying
+print("I study for 1 hour at night.")
+
+# Calculating total study time in a week
+print("Total study hours in a week:", 1 * 7)
