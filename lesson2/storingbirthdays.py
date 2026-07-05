76f690c8bc6d102900966b9b81c042c06b5eebd1
soham <getsoham.j12@gmail.com>
2026-07-05 18:51:43 -0500

    lesson_2

---
+++ lesson2/storingbirthdays.py
@@ -0,0 +1,11 @@
+friend_1 = "Aarav - January 15"
+friend_2 = "Arnav - March 22"
+friend_3 = "Kabir - July 8"
+friend_4 = "Vivaan - September 14"
+friend_5 = "Rishan - December 5"
+
+print(friend_1)
+print(friend_2)
+print(friend_3)
+print(friend_4)
+print(friend_5)
