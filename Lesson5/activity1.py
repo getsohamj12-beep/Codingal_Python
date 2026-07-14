b00b900c5ebd60aae741899a8efa36362c8254c1
soham <getsoham.j12@gmail.com>
2026-07-13 19:36:11 -0500

    Lesson5 Co-authored-by: Biswareet Panda <biswa.gunu2003@gmail.com>

---
+++ Lesson5/activity1.py
@@ -0,0 +1,6 @@
+num= 14
+if num<30:
+     print ("num is less than 30")
+num=34
+if num>35:
+     print("something")
---
+++ Lesson5/activity4.py
@@ -0,0 +1,5 @@
+x=int(input("enter a number"))
+if (x%2==0):
+    print("Even number")
+else:
+    print("Odd Number")
